43eaec899d706506b17b8d5c4cd09859a3014c48
Anindita Nandy <40290536+trayee-96@users.noreply.github.com>
2020-09-21 20:18:48 +0530

    Add files via upload

---
+++ GOF-FacadePatternHandsOn/src/FacedPatternDemo.java
@@ -0,0 +1,11 @@
+
+public class FacedPatternDemo {
+
+	public static void main(String[] args) {
+		ShapeMaker shapeMaker = new ShapeMaker();
+		shapeMaker.drawCircle();
+		shapeMaker.drawRectangle();
+		shapeMaker.drawSquare();
+	}
+
+}
